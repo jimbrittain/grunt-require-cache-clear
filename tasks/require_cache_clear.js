b23a1d7c96e25161bb9080961301af2c0ae7bdcf
Jim Brittain <jim@immaturedawn.co.uk>
2016-12-13 11:03:38 +0000

    Still Trying

--- tasks/require_cache_clear.js
+++ tasks/require_cache_clear.js
@@ -13,14 +13,17 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('require_cache_clear', 'Removal of files from require.cache where retention can cause issues in watch environments.', function(arg1, arg2) {
+  grunt.registerMultiTask('require_cache_clear', 'Removal of files from require.cache where retention can cause issues in watch environments.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     if(require && 'cache' in require){
         //arg1 = arg1 || (this.data.files.src || undefined);
         //arg2 = arg2 || (this.data.options || undefined);
-        arg1 = this.data || undefined;
+        var arg1 = this.data || undefined;
         var rcc;
         if(arg1 !== undefined){
+            for(var prop in arg1){
+                grunt.log.writeln(prop + ' = ' + arg1[prop]);
+            }
             rcc = new RequireCacheClear(arg1);
         //var rcc = new RequireCacheClear(arg1, arg2);
         var l = rcc.run(), i, imax;
