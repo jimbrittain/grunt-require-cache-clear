77ba223167b841c67c2f02cbf2711ea99179277c
Jim Brittain <jim@immaturedawn.co.uk>
2019-09-29 22:29:14 +0100

    Updated to correct spawn related issue in with grunt

--- tasks/require_cache_clear.js
+++ tasks/require_cache_clear.js
@@ -2,38 +2,32 @@
  * grunt-require-cache-clear
  * https://github.com/jimbrittain/grunt-require-cache-clear
  *
- * Copyright (c) 2016 Jim Brittain
+ * Copyright (c) 2019 Jim Brittain
  * Licensed under the MIT license.
  */
-
-'use strict';
-var RequireCacheClear = require('../lib/RequireCacheClear').RequireCacheClear;
+"use strict";
 module.exports = function(grunt) {
-
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
+  var RequireCacheClear = require('../lib/RequireCacheClear').RequireCacheClear;
   grunt.registerMultiTask('require_cache_clear', 'Removal of files from require.cache where retention can cause issues in watch environments.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    if(require && 'cache' in require){
-        //arg1 = arg1 || (this.data.files.src || undefined);
-        //arg2 = arg2 || (this.data.options || undefined);
-        var arg1 = this.data || undefined;
-        var rcc;
-        if(arg1 !== undefined){
-            for(var prop in arg1){
-                grunt.log.writeln(prop + ' = ' + arg1[prop]);
-            }
-            rcc = new RequireCacheClear(arg1);
-        //var rcc = new RequireCacheClear(arg1, arg2);
-        var l = rcc.run(), i, imax;
-        for(i=0, imax = l.msg.length; i<imax; i+=1){ grunt.log.writeln(l.msg[i]); }
-        for(i=0, imax = l.errors.msg.length; i<imax; i+=1){ grunt.log.error(l.errors.msg[i]); }
-        for(i=0, imax = l.verbose.length; i<imax; i+=1){ grunt.log.verbose.writeln(l.verbose[i]); }
-        for(i=0, imax = l.errors.verbose.length; i<imax; i+=1){ grunt.log.verbose.error(l.errors.verbose[i]); }
+        // Merge task-specific and/or target-specific options with these defaults.
+        if(require && 'cache' in require){
+            var files = this.data.files,
+                options = this.data.options; 
+            files = files || (this.files || undefined);
+            options = options || (this.options || undefined);
+
+            var rcc, i, imax, l;
+            if (files !== undefined && 'src' in files) {
+                for (i=0, imax = files.length; i<imax; i+=1) {
+                    grunt.log.writeln('Searching require.cache for ' + files.src[i]); }}
+            rcc = new RequireCacheClear(files, options);
+            l = rcc.run();
+            for(i=0, imax = l.msg.length; i<imax; i+=1){ grunt.log.writeln(l.msg[i]); }
+            for(i=0, imax = l.errors.msg.length; i<imax; i+=1){ grunt.log.error(l.errors.msg[i]); }
+            for(i=0, imax = l.verbose.length; i<imax; i+=1){ grunt.log.verbose.writeln(l.verbose[i]); }
+            for(i=0, imax = l.errors.verbose.length; i<imax; i+=1){ grunt.log.verbose.error(l.errors.verbose[i]); }
+        } else {
+            grunt.log.error('require.cache cannot be found. Exiting with error.');
         }
-    } else {
-        grunt.log.error('require.cache cannot be found. Exiting with error.');
-    }
     });
 };
